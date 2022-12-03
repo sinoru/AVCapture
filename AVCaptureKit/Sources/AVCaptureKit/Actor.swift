e5216576456d9bd195e7b7081f42fb23d469ba32
Jaehong Kang <sinoru@me.com>
2022-12-03 14:26:07 +0900

    Improves Actor helpers

--- AVCaptureKit/Sources/AVCaptureKit/Actor.swift
+++ AVCaptureKit/Sources/AVCaptureKit/Actor.swift
@@ -6,7 +6,19 @@
 //
 
 extension Actor {
-    func run<T>(resultType: T.Type = T.self, body: @Sendable (isolated Self) async throws -> T) async rethrows -> T where T : Sendable {
+    public func run<T>(resultType: T.Type = T.self, body: @Sendable (isolated Self) throws -> T) async rethrows -> T where T : Sendable {
+        try body(self)
+    }
+
+    @_disfavoredOverload
+    public func run<T>(resultType: T.Type = T.self, body: @Sendable (isolated Self) async throws -> T) async rethrows -> T where T : Sendable {
         try await body(self)
+    }
+}
+
+extension MainActor {
+    @_disfavoredOverload
+    public static func run<T>(resultType: T.Type = T.self, body: @MainActor @Sendable () async throws -> T) async rethrows -> T where T : Sendable {
+        try await body()
     }
 }
